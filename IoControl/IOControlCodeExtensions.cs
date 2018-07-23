b8a8dcea6a7ef1cfd1213240cde709d495687def
juner <juner@juner.net>
2018-07-24 08:49:59 +0900

    CTL_CODE 周りの修正 及び テストの充実

--- IoControl/IOControlCodeExtensions.cs
+++ IoControl/IOControlCodeExtensions.cs
@@ -5,15 +5,66 @@ namespace IoControl
 {
     public static class IOControlCodeExtensions
     {
+        /// <summary>
+        /// CTL_CODE
+        /// </summary>
+        /// <param name="DeviceType"></param>
+        /// <param name="Function"></param>
+        /// <param name="Method"></param>
+        /// <param name="Access"></param>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IOControlCode CtrlCode(FileDevice DeviceType, uint Function, Method Method, FileAccess Access)
+        public static IOControlCode CtrlCode(FileDevice DeviceType, uint Function = default, Method Method = default, FileAccess Access = default)
        => (IOControlCode)CtrlCode((uint)DeviceType, Function, (uint)Method, (uint)Access);
+        /// <summary>
+        /// CTL_CODE
+        /// </summary>
+        /// <param name="DeviceType"></param>
+        /// <param name="Function"></param>
+        /// <param name="Method"></param>
+        /// <param name="Access"></param>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint CtrlCode(uint DeviceType, uint Function, uint Method, uint Access)
+        private static uint CtrlCode(uint DeviceType, uint Function = default, uint Method = default, uint Access = default)
             => (DeviceType << 16) | (Function << 2) | Method | (Access << 14);
-        public static (FileDevice DeviceType, uint Function, Method Method, FileAccess Access) UnCtrlCode(uint CtrlCode)
-            => ((FileDevice)((CtrlCode & 0xffff_0000) >> 16), (CtrlCode & 0x0000_3FFC) >> 2, (Method)((CtrlCode & 0x0000_0003) >> 2), (FileAccess)((CtrlCode & 0x0000_C000) >> 14));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CtrlCode"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FileDevice GetDeviceType(this IOControlCode CtrlCode) => (FileDevice)(((uint)CtrlCode & 0xffff_0000) >> 16);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CtrlCode"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint GetFunction(this IOControlCode CtrlCode) => ((uint)CtrlCode & 0x0000_3FFC) >> 2;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CtrlCode"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Method GetMethod(this IOControlCode CtrlCode) => (Method)(((uint)CtrlCode & 0x0000_0003) >> 2);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CtrlCode"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FileAccess GetAccess(this IOControlCode CtrlCode) => (FileAccess)(((uint)CtrlCode & 0x0000_C000) >> 14);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CtrlCode"></param>
+        /// <param name="DeviceType"></param>
+        /// <param name="Function"></param>
+        /// <param name="Method"></param>
+        /// <param name="Access"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Deconstruct(this IOControlCode CtrlCode, out FileDevice DeviceType, out uint Function, out Method Method, out FileAccess Access)
-            => (DeviceType, Function, Method, Access) = UnCtrlCode((uint)CtrlCode);
+            => (DeviceType, Function, Method, Access) = (GetDeviceType(CtrlCode), GetFunction(CtrlCode), GetMethod(CtrlCode), GetAccess(CtrlCode));
     }
 }
--- IoControlTests/IOControlCodeExtensionsTest.cs
+++ IoControlTests/IOControlCodeExtensionsTest.cs
@@ -2,17 +2,41 @@
 using IoControl;
 using System.IO;
 
-namespace IoControlTests
+namespace IoControl.Tests
 {
     [TestClass]
     public class IOControlCodeExtensionsTest
     {
         [TestMethod]
+        public void CtrlCodeTest()
+        {
+            Assert.AreEqual(IOControlCodeExtensions.CtrlCode(FileDevice.Controller, 0x040bu, Method.Buffered, FileAccess.ReadWrite), IOControlCode.AtaPassThrough);
+        }
+        [TestMethod]
+        public void GetDeviceTypeTest()
+        {
+            Assert.AreEqual(IOControlCode.AtaPassThrough.GetDeviceType(), FileDevice.Controller);
+        }
+        [TestMethod]
+        public void GetFunctionTest()
+        {
+            Assert.AreEqual(IOControlCode.AtaPassThrough.GetFunction(), 0x040bu);
+        }
+        [TestMethod]
+        public void GetMethodTest()
+        {
+            Assert.AreEqual(IOControlCode.AtaPassThrough.GetMethod(), Method.Buffered);
+        }
+        public void GetAccessTest()
+        {
+            Assert.AreEqual(IOControlCode.AtaPassThrough.GetAccess(), FileAccess.ReadWrite);
+        }
+        [TestMethod]
         public void DeconstructTest()
         {
             var (DeviceType, Function, Method, Access) = IOControlCode.AtaPassThrough;
             Assert.AreEqual(DeviceType, FileDevice.Controller);
-            Assert.AreEqual(Function, (uint)0x040b);
+            Assert.AreEqual(Function, 0x040bu);
             Assert.AreEqual(Method, Method.Buffered);
             Assert.AreEqual(Access, FileAccess.ReadWrite);
         }
