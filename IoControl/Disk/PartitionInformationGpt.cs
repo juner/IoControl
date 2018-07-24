149b1be78d2eb96c04262119c9bcfa85c0fe4186
jun maeda <j.maeda@activeltd.co.jp>
2018-07-24 09:55:25 +0900

    ParititionInformationGpt の Guid値が正しくない値をとっていた 為、 pinvoke.net に合わせて修正

--- IoControl/Disk/PartitionInformationGpt.cs
+++ IoControl/Disk/PartitionInformationGpt.cs
@@ -3,17 +3,13 @@ using System.Runtime.InteropServices;
 
 namespace IoControl.Disk
 {
-    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode, Pack = 4)]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Pack = 4)]
     public struct PartitionInformationGpt
     {
-        [FieldOffset(0)]
         public Guid PartitionType;
-        [FieldOffset(16)]
         public Guid PartitionId;
-        [FieldOffset(32)]
         [MarshalAs(UnmanagedType.U8)]
         public EFIPartitionAttributes Attributes;
-        [FieldOffset(40)]
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 36)]
         public string Name;
         public override string ToString() => $"{nameof(PartitionInformationGpt)}:{{" +
