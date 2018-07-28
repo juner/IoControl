0d7ea37001cf60862d482eb50cf30fcdd52e07f9
jun maeda <j.maeda@activeltd.co.jp>
2018-07-28 17:42:41 +0900

    PartitionInformaitonUnion を 使わない形にすることで対応。 fix #1 Explict は String とか class を交えるとMarshal.SizeOf でエラーとなる為、既存で用意しているプロパティにて PtrToStructure する対応でどうにか解決させた。 それに伴い、同Unionとそのテストの削除

--- IoControl/Disk/PartitionInformationEx.cs
+++ IoControl/Disk/PartitionInformationEx.cs
@@ -11,9 +11,29 @@ namespace IoControl.Disk
         public long PartitionLength;
         public uint PartitionNumber;
         public bool RewritePartition;
-        public PartitionInformationUnion Info;
-        public PartitionInformationMbr Mbr { get => Info.Mbr; set => Info.Mbr = value; }
-        public PartitionInformationGpt Gpt { get => Info.Gpt; set => Info.Gpt = value; }
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 112)]
+        public byte[] Info;
+        public PartitionInformationMbr Mbr { get => ToStructure<PartitionInformationMbr>(Info); set => FromStructure(Info, value); }
+        public PartitionInformationGpt Gpt { get => ToStructure<PartitionInformationGpt>(Info); set => FromStructure(Info, value); }
+        private static T ToStructure<T>(byte[] Bytes, int StartIndex = default){
+            var Size = Marshal.SizeOf<T>();
+            var Ptr = Marshal.AllocCoTaskMem(Size);
+            using (Disposable.Create(() => Marshal.FreeCoTaskMem(Ptr)))
+            {
+                Marshal.Copy(Bytes, StartIndex, Ptr, Size);
+                return (T)Marshal.PtrToStructure(Ptr, typeof(T));
+            }
+        }
+        private static void FromStructure<T>(byte[] Bytes, T Structure, int StartIndex = default)
+        {
+            var Size = Marshal.SizeOf<T>();
+            var Ptr = Marshal.AllocCoTaskMem(Size);
+            using (Disposable.Create(() => Marshal.FreeCoTaskMem(Ptr)))
+            {
+                Marshal.StructureToPtr(Structure, Ptr, false);
+                Marshal.Copy(Ptr, Bytes, StartIndex, Size);
+            }
+        }
         public override string ToString()
         {
             return $"{nameof(PartitionInformationEx)}{{" +
