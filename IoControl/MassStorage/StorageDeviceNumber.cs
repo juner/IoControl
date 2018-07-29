2c96f9c740a539fb76bbfde630d761068eb965c6
jun maeda <j.maeda@activeltd.co.jp>
2018-07-29 12:34:03 +0900

    テストプログラム修正

--- IoControl/MassStorage/StorageDeviceNumber.cs
+++ IoControl/MassStorage/StorageDeviceNumber.cs
@@ -1,10 +1,16 @@
 ﻿using System.Runtime.InteropServices;
-
 namespace IoControl.MassStorage
 {
+    /// <summary>
+    /// STORAGE_DEVICE_NUMBER structure ( https://docs.microsoft.com/ja-jp/windows/desktop/api/winioctl/ns-winioctl-_storage_device_number )
+    /// Contains information about a device. This structure is used by the <see cref="IOControlCode.StorageGetDeviceNumber"/> control code.
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct StorageDeviceNumber
     {
+        /// <summary>
+        /// The type of device. Values from 0 through 32,767 are reserved for use by Microsoft. Values from 32,768 through 65,535 are reserved for use by other vendors. The following values are defined by Microsoft:
+        /// </summary>
         public FileDevice DeviceType;
         public uint DeviceNumber;
         public uint PartitionNumber;
--- IoControlPhysicalExample/Program.cs
+++ IoControlPhysicalExample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using IoControl;
 using IoControl.Disk;
 using IoControl.Controller;
+using IoControl.MassStorage;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,11 +22,19 @@ namespace IoControlPhysicalExample
                     CreationDisposition: FileMode.Open,
                     FlagAndAttributes: FileAttributes.Normal))
             {
-
                 try
                 {
-                    Trace.WriteLine(nameof(IOControlCode.DiskGetDriveGeometryEx));
-                    IoControl.DiskGetDriveGeometryEx(out var geometry);
+                    Trace.WriteLine(nameof(MassStorageExtensions.StorageGetDeviceNumber));
+                    IoControl.StorageGetDeviceNumber(out var number);
+                    Trace.WriteLine(number);
+                }catch(Exception e)
+                {
+                    Trace.WriteLine(e);
+                }
+                try
+                {
+                    Trace.WriteLine(nameof(DiskExtensions.DiskGetDriveGeometryEx2));
+                    IoControl.DiskGetDriveGeometryEx2(out var geometry);
                     Trace.WriteLine(geometry);
                 }
                 catch (Exception e)
@@ -109,12 +118,12 @@ namespace IoControlPhysicalExample
         public static IEnumerable<IoControl.IoControl> GetPhysicalDrives(FileAccess FileAccess = default, FileShare FileShare = default, FileMode CreationDisposition = default, FileAttributes FlagAndAttributes = default)
         {
             bool hasDrive = false;
-            foreach (var PhysicalNumber in Enumerable.Range(0, 10))
+            foreach (var DeviceName in IoControl.Utils.DeviceUtils.QueryDocDevice().Where(v => v.IndexOf("PhysicalDrive") == 0))
             {
-                var Path = $@"\\.\PhysicalDrive{PhysicalNumber}";
+                var Path = $@"\\.\{DeviceName}";
                 using (var file = new IoControl.IoControl(Path, FileAccess, FileShare, CreationDisposition, FlagAndAttributes))
                 {
-                    Trace.WriteLine($"Open {Path} ... {(file.IsInvalid ? "NG" : "OK")}.");
+                    Trace.WriteLine($"Open {file} ... {(file.IsInvalid ? "NG" : "OK")}.");
                     if (file.IsInvalid)
                         continue;
                     hasDrive = true;
