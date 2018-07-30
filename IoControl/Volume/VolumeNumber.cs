0c8c7d413342b85ef7c6a2928acafb9aa7cba058
jun maeda <j.maeda@activeltd.co.jp>
2018-07-30 09:40:23 +0900

    構造体名と被っていた為VolumeNum  にしていたが、中途半端なのでNumber に変更

--- IoControl/Volume/VolumeNumber.cs
+++ IoControl/Volume/VolumeNumber.cs
@@ -12,7 +12,7 @@ namespace IoControl.Volume
         /// <summary>
         /// Specifies the  number associated with this volume for the current session.
         /// </summary>
-        public uint VolumeNum;
+        public uint Number;
         /// <summary>
         /// Specifies the name of the volume manager driver.  If this is less than 8, it is padded with blanks.
         /// </summary>
@@ -24,15 +24,15 @@ namespace IoControl.Volume
         /// <param name="VolumeNumber"></param>
         /// <param name="VolumeManagerName"></param>
         public VolumeNumber(uint VolumeNumber, ushort[] VolumeManagerName)
-            => (VolumeNum, this.VolumeManagerName) = (VolumeNumber, VolumeManagerName);
+            => (Number, this.VolumeManagerName) = (VolumeNumber, VolumeManagerName);
         /// <summary>
         /// 
         /// </summary>
         /// <param name="VolumeNumber"></param>
         /// <param name="VolumeManagerName"></param>
         public void Deconstruct(out uint VolumeNumber, out ushort[] VolumeManagerName)
-            => (VolumeNumber, VolumeManagerName) = (this.VolumeNum, this.VolumeManagerName);
+            => (VolumeNumber, VolumeManagerName) = (Number, this.VolumeManagerName);
         public override string ToString()
-            => $"{nameof(VolumeNumber)}{{{nameof(VolumeNum)}:{VolumeNum}, {nameof(VolumeManagerName)}:[{string.Join("", (VolumeManagerName ?? Enumerable.Empty<ushort>()).Select(v => char.ConvertFromUtf32(v)))}]}}";
+            => $"{nameof(VolumeNumber)}{{{nameof(Number)}:{Number}, {nameof(VolumeManagerName)}:[{string.Join("", (VolumeManagerName ?? Enumerable.Empty<ushort>()).Select(v => char.ConvertFromUtf32(v)))}]}}";
     }
 }
