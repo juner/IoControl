de2280519045d4defba9782f15ae08f639c35d3c
jun maeda <j.maeda@activeltd.co.jp>
2018-07-31 14:17:13 +0900

    カレントディレクトリはマウント済みなので、それを元にしたチェックを追加

--- IoControlTests/FileSystem/FileSystemExtensionsTest.cs
+++ IoControlTests/FileSystem/FileSystemExtensionsTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,23 @@ namespace IoControl.FileSystem.Tests
     [TestClass]
     public class FileSystemExtensionsTest
     {
-        private static IEnumerable<object[]> FsctlIsVolumeMountedTestData => VolumePathGenerator().Concat(PhysicalDrivePathGenerator()).Concat(LogicalDrivePathGenerator())
-            .GetIoControls(FileAccess: System.IO.FileAccess.Read, FileShare: System.IO.FileShare.Read | System.IO.FileShare.Write, CreationDisposition: System.IO.FileMode.Open).Select(v => new object[] { v });
+        private static IEnumerable<object[]> FsctlIsVolumeMountedTestData {
+            get {
+                using (var IoControl = GetIoControl($@"\\.\{Path.GetPathRoot(Directory.GetCurrentDirectory()).TrimEnd('\\')}", FileAccess: FileAccess.Read, FileShare: FileShare.Read | FileShare.Write, CreationDisposition: FileMode.Open))
+                    yield return new object[] { IoControl, true };
+                foreach (var path in VolumePathGenerator().Concat(PhysicalDrivePathGenerator()).Concat(LogicalDrivePathGenerator())
+                .GetIoControls(FileAccess: FileAccess.Read, FileShare: FileShare.Read | FileShare.Write, CreationDisposition: FileMode.Open))
+                    yield return new object[] { path, null };
+            }
+        }
         [TestMethod]
         [DynamicData(nameof(FsctlIsVolumeMountedTestData))]
-        public void FsctlIsVolumeMountedTest(IoControl IoControl) => Trace.WriteLine(IoControl.FsctlIsVolumeMounted());
+        public void FsctlIsVolumeMountedTest(IoControl IoControl, bool? result)
+        {
+            var _result = IoControl.FsctlIsVolumeMounted();
+            Trace.WriteLine(_result);
+            if (result is bool Result)
+                Assert.AreEqual(Result, _result);
+        }
     }
 }
